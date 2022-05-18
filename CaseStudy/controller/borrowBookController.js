828ae10c0d780b61c4423957a1b234c59f378a86
Nam <hakhanhnam.qb@gmail.com>
2022-05-18 16:31:11 +0700

    Signed-off-by: Nam <hakhanhnam.qb@gmail.com>

--- CaseStudy/controller/borrowBookController.js
+++ CaseStudy/controller/borrowBookController.js
@@ -1,8 +1,8 @@
 const bookModel = require('../model/bookModel');
 const borrowBookModel = require('../model/borrowBokModel');
 exports.borrowBook = async (req, res, next) => {
-    console.log(req.body);
-    const books = await bookModel.findOne({ bookName: req.body.bookName });
+    // console.log(req.body);
+    let books = await bookModel.findOne({ _id: req.body.bookID });
     if (books) {
         const borrowBookData = {
             nameStudent: req.body.nameStudent,
@@ -12,11 +12,25 @@ exports.borrowBook = async (req, res, next) => {
             borrowDate: req.body.borrowDate,
             returnDate: null
         }
-        const borrowing = await bookModel.create(borrowBookData);
+        const borrowing = await borrowBookModel.create(borrowBookData);
         if (borrowing) {
+            books.quantity_borrow += 1;
+            books.quantity_remain -= 1;
+            await books.save();
+            // await bookModel.updateOne(books);
             res.json({ borrowing: borrowing });
+            console.log(await bookModel.findOne({ _id: req.body.bookID }), "book model");
         }
+
     } else {
         res.json("Book " + req.body.bookName + " does not exist");
     }
+}
+exports.returnBook = async (req, res, next) => {
+    const borrowed = await borrowBookModel.find({ studentID: req.body.studentID, returnDate: null });
+    // console.log(borrowed);
+    res.render('returnBook',{borrowed: borrowed});
+    await borrowBookModel.findOne({_id: id}).then({
+        
+    })
 }
--- CaseStudy/routes/borrowBookRoute.js
+++ CaseStudy/routes/borrowBookRoute.js
@@ -2,15 +2,17 @@ const express = require('express');
 const route = express.Router();
 const multer = require('multer');
 const upload = multer();
-const libraryModel =  require('../model/libraryModel');
+const libraryModel = require('../model/libraryModel');
 const bookModel = require('../model/bookModel');
+const borrowBookModel = require('../model/borrowBokModel');
 const borrowBookController = require('../controller/borrowBookController');
 route.get('/', async (req, res) => {
     const allBooks = await bookModel.find();
-    console.log(allBooks);
-    res.render('borrowBook',{allBooks: allBooks})
+    // console.log(allBooks);
+    res.render('borrowBook', { allBooks: allBooks })
 });
+route.post('/borrowBooks', upload.none(), borrowBookController.borrowBook);
 
-route.post('/borrowBooks', upload.none(),borrowBookController.borrowBook);
-
+route.get('/searchStudentID', (req,res)=>{res.render('searchStudentID')});
+route.post('/returnBook', upload.none(), borrowBookController.returnBook);
 module.exports = route;
